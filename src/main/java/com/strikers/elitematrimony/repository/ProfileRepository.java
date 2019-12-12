e646a71b78d0280212212d11fb9341194812fb59
Shilendra <shuilu1993sr@gmail.com>
2019-12-12 16:07:56 +0530

    ProfileMapping for interested/accepted

--- src/main/java/com/strikers/elitematrimony/repository/ProfileRepository.java
+++ src/main/java/com/strikers/elitematrimony/repository/ProfileRepository.java
@@ -1,18 +1,5 @@
 package com.strikers.elitematrimony.repository;
 
-<<<<<<< HEAD
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.strikers.elitematrimony.entity.Profile;
-import com.strikers.elitematrimony.entity.ProfileMapping;
-
-@Repository
-public interface ProfileRepository extends JpaRepository<Profile, Integer>{
-
-	Profile findByProfileId(Integer profileId);
-
-=======
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -33,6 +20,7 @@ public interface ProfileRepository extends JpaRepository<Profile, Integer> {
 	 */
 	@Query("select p from Profile p where p.language like %:searchKey% or p.qualification like %:searchKey% or p.maritalStatus like %:searchKey% or p.qualification like %:searchKey% or p.hobby like %:searchKey% or p.city like %:searchKey%")
 	List<Profile> searchProfile(@Param("searchKey") String searchKey);
->>>>>>> 204cbe0af0575b6e3de6529facfecb145118719f
+	
+	Profile findByProfileId(Integer profileId);
 
 }
